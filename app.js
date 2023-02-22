7e4301b5ae46021ed7c7f74585556b5c9a2fc3db
omer evron <omerevron011@gmail.com>
2023-02-22 13:06:48 +0200

    final proto manual zoom

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ async function draw () {
         d3.autoType(d)
         d.total = d3.sum(columns, (c) => d[c])
         d.title = d['']
+        // this.size = index
         return d
       })
 
@@ -21,52 +22,70 @@ async function draw () {
     }
     dimensions.ctrHeight = dimensions.height - (dimensions.margin.bottom*2)
     dimensions.ctrWidth = dimensions.height - dimensions.margin.bottom*2
+
+
+    const xAccessor = (d,i) => i 
+    const yAccessor = (d,i) => d.total
     
+    const xScale = d3.scaleLinear().domain(d3.extent(dataset,xAccessor)).rangeRound([50,dimensions.ctrWidth]).clamp(true)
+    const yScale = d3.scaleLinear().domain(d3.extent(dataset,yAccessor)).rangeRound([dimensions.ctrHeight,0]).nice().clamp(true) 
+
         const svg = d3.select('#chart')
         .append('svg')
         .attr('height',dimensions.height)
         .attr('width',dimensions.width)
-        .attr('viewBox',`0 0 ${dimensions.height} ${dimensions.width}`)
+        .attr('viewBox',`${-dimensions.width/2} ${-dimensions.height/2} ${dimensions.height} ${dimensions.width}`)
+        // ${-dimensions.width/2} ${-dimensions.height/2}
         .style('background-color','#ddd')
     
         const ctr = svg.append('g')
         .attr('translate',`transform(${dimensions.margin.left},${dimensions.margin.top})`)
-
+        .classed('container',true)
+        const circlesGroup = ctr.append('g')
         //Scales
 
-        const sizeScale = (data=[]) =>d3.scaleLinear()
-        .domain([1,data.length])
-        .range([dimensions.margin.top,dimensions.width - 50])
-        const groupAccessor = (d,i) => {
-                // let obj = {
-                //     index: i,
-                //     object:d.title,
-                //     currentVal:d[i]
-                // }
-                // console.log(obj);
-                // return d
-        }
+        const sizeScale =d3.scaleLog()
+        .domain([1,56])
+        .range([dimensions.margin.bottom,dimensions.ctrWidth])
+
+        //Intial Group
+
+        circlesGroup.selectAll('circle')
+        .data(dataset)
+        .join('circle')
+        .attr('r',d=>sizeScale(d.length))
         
-            //Fire Change And Join data
+        //Fire Change And Join data
+
+        
             let newData;
-         d3.select('#group')
-        // .value('value')
-        .on('change',function(event){
-                let value = 0
-                value = event.target.value
+            d3.select('#group')
+            .on('change',function(event){
+
+            let value = 0
+            value = event.target.value
+
                 const bin = d3.bin()
-                // .domain([])
                 .value((d)=>d[value])
                 .thresholds(value)
+
             newData = bin(dataset)
-            ctr.selectAll('circle')
+
+            circlesGroup.selectAll('circle')
             .data(newData)
-            .join(enter=>{},update=>{},exit =>{})
+            .join('circle')
+            .attr('r', (d,i)=>sizeScale(d.length)*0.1)
+            .transition()
+            // .attr('cx',(d,i)=>sizeScale(i))
+            .attr('cx', (d,i)=>xScale(xAccessor(d,i)))
+            .attr('cy',(d,i)=>yScale(yAccessor(i)))
+            .attr('fill',(d,i)=>d3.interpolatePurples(i/56))
             console.log(newData);
-            })
+        
+        })
 
 
-    // console.log(value);
+    // console.log(dataset);
 
 
 }
